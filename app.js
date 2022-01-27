370539d5494cc2fcddc587428020515ba8c3fc92
AxelMartin69 <75273577+AxelMartin69@users.noreply.github.com>
2022-01-27 23:13:42 +0200

    0. Kokkuvõtte - module pattern

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-// module pattern basic structure
-(function(){
-    // declare private variables and functions
+// revealing module pattern
+// it will be Item Controller
+const ItemController = (function(){
+    let data = []
+
+    function add (item){
+        data.push(item)
+        console.log('Item is added');
+    }
+
+    function get (id) {
+        return data.find(item => {
+            return item.id === id
+        })
+    }
 
     return {
-        // declare public variables and functions
+        add: add,
+        get: get
     }
-})
+
+})();
+
+ItemController.add({id:1, name:'Kate'})
+console.log(ItemController.get(1));
